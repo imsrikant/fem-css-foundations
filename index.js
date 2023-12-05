600e7d16cdcf44479cebcdb964b4699e70d1c3f9
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2023-12-05 13:21:25 +0000

    Fix menu and close button behavior

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
 const nav = document.querySelector(".nav");
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
+const body = document.querySelector(".body");
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("nav--visible");
     menu.classList.toggle("menu--hide");
     close.classList.toggle("close--show");
+    body.classList.toggle("body--no-scroll");
 });
 
 close.addEventListener("click", () => {
     nav.classList.toggle("nav--visible");
     menu.classList.toggle("menu--hide");
     close.classList.toggle("close--show");
+    body.classList.toggle("body--no-scroll");
 });
